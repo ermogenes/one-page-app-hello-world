3ac495fdd2eee95e368dc966eddcded2224e1a35
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-10-15 10:17:43 -0300

    Teste finalizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Tudo funcionando corretamente.');
+import Router from 'vanilla-router';
+
+const showMessage = (markup) => {
+  document.getElementById('message-box').innerHTML = markup;
+};
+
+const router = new Router({
+  mode: 'history',
+  page404: (path) => showMessage(`Página não encontrada: /${path}`),
+});
+
+const handleNavigate = (event) => {
+  event.preventDefault();
+  const url = new URL(event.currentTarget.href);
+  router.navigateTo(url.pathname);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('a').forEach((a) => {
+    a.addEventListener('click', handleNavigate);
+  });
+
+  router
+    .add('/', () => showMessage('Você acessou a página inicial.'))
+    .add('/hello/{parameter}', (parameter) => showMessage(`Hello ${parameter}`))
+    .add('/about', () => showMessage('Você acessou a página "Sobre".'))
+    .addUriListener();
+});
